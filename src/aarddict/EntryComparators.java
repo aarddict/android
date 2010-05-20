18a42a13a4bc10871911d9acee7e4b82ddedb7d0
Igor Tkach <itkach@aarddict.org>
2010-05-20 19:50:03 -0400

    Remove QUATERNARY comparators

--- src/aarddict/EntryComparators.java
+++ src/aarddict/EntryComparators.java
@@ -9,30 +9,28 @@ public class EntryComparators {
 	final static EntryComparator FULL1 = new EntryComparator(Collator.PRIMARY);
 	final static EntryComparator FULL2 = new EntryComparator(Collator.SECONDARY);
 	final static EntryComparator FULL3 = new EntryComparator(Collator.TERTIARY);
-	final static EntryComparator FULL4 = new EntryComparator(Collator.QUATERNARY);
 
 	final static EntryStartComparator START1 = new EntryStartComparator(Collator.PRIMARY);
 	final static EntryStartComparator START2 = new EntryStartComparator(Collator.SECONDARY);
 	final static EntryStartComparator START3 = new EntryStartComparator(Collator.TERTIARY);
-	final static EntryStartComparator START4 = new EntryStartComparator(Collator.QUATERNARY);
 	
 	@SuppressWarnings("unchecked")
 	public static Comparator<Entry>[] ALL = new Comparator[] {
-		FULL4, FULL3, FULL2, FULL1, START4, START3, START2, START1
+		FULL3, FULL2, FULL1, START3, START2, START1
 	};
 	
 	@SuppressWarnings("unchecked")
 	public static Comparator<Entry>[] ALL_FULL = new Comparator[] {
-		FULL4, FULL3, FULL2, FULL1
+		FULL3, FULL2, FULL1
 	};
 
 	@SuppressWarnings("unchecked")
 	public static Comparator<Entry>[] EXACT_IGNORE_CASE = new Comparator[] {
-		FULL4, FULL3, FULL2
+		FULL3, FULL2
 	};		
 	
 	@SuppressWarnings("unchecked")
 	public static Comparator<Entry>[] EXACT = new Comparator[] {
-		FULL4, FULL3
+		FULL3
 	};		
 }

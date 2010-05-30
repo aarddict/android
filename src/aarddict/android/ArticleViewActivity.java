ae900a3b495522ec2ee924c3a5d44ada40b0360d
Igor Tkach <itkach@aarddict.org>
2010-05-30 00:09:22 -0400

    Swap 'Zoom Out' and 'Zoom In' menu positions

--- src/aarddict/android/ArticleViewActivity.java
+++ src/aarddict/android/ArticleViewActivity.java
@@ -373,8 +373,8 @@ public class ArticleViewActivity extends Activity {
         miNextArticle = menu.add(0, MENU_NEXT, 0, "Next").setIcon(android.R.drawable.ic_media_next);
         miViewOnline = menu.add(0, MENU_VIEW_ONLINE, 0, "View Online").setIcon(android.R.drawable.ic_menu_view);
         menu.add(0, MENU_NEW_LOOKUP, 0, "New Lookup").setIcon(android.R.drawable.ic_menu_search);        
-        menu.add(0, MENU_ZOOM_IN, 0, "Zoom In").setIcon(android.R.drawable.btn_plus);
         menu.add(0, MENU_ZOOM_OUT, 0, "Zoom Out").setIcon(android.R.drawable.btn_minus);
+        menu.add(0, MENU_ZOOM_IN, 0, "Zoom In").setIcon(android.R.drawable.btn_plus);
         return true;
     }
     

42a431efcc0b853fb88dcca902c9c3fa26f4fd28
Igor Tkach <itkach@aarddict.org>
2010-06-02 17:26:37 -0400

    Re-add hashCode()/equals() in Entry

--- src/aarddict/Entry.java
+++ src/aarddict/Entry.java
@@ -50,5 +50,46 @@ public final class Entry {
 		this.volumeId = volumeId;
 	}
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + (int) (articlePointer ^ (articlePointer >>> 32));
+        result = prime * result + ((section == null) ? 0 : section.hashCode());
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        result = prime * result
+                + ((volumeId == null) ? 0 : volumeId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Entry other = (Entry) obj;
+        if (articlePointer != other.articlePointer)
+            return false;
+        if (section == null) {
+            if (other.section != null)
+                return false;
+        } else if (!section.equals(other.section))
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        if (volumeId == null) {
+            if (other.volumeId != null)
+                return false;
+        } else if (!volumeId.equals(other.volumeId))
+            return false;
+        return true;
+    }
 
 }

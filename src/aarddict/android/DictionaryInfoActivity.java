daa4a68896a7511c1930dce7be4449bad3d390fb
Igor Tkach <itkach@aarddict.org>
2010-07-29 00:36:12 -0400

    Add header comment to all source files

--- src/aarddict/android/DictionaryInfoActivity.java
+++ src/aarddict/android/DictionaryInfoActivity.java
@@ -1,3 +1,18 @@
+/* This file is part of Aard Dictionary for Android <http://aarddict.org>.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License <http://www.gnu.org/licenses/gpl-3.0.txt>
+ * for more details.
+ * 
+ * Copyright (C) 2010 Igor Tkach
+*/
+
 package aarddict.android;
 
 import aarddict.Metadata;

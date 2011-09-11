5d79aa7fe087f11f7a406721dccec90e86ccfcc5
Igor Tkach <itkach@aarddict.org>
2011-09-11 13:06:21 -0400

    Use volume keys to scroll and move to next/previous article instead of zoom in/zoom out

--- src/aarddict/android/ArticleViewActivity.java
+++ src/aarddict/android/ArticleViewActivity.java
@@ -283,10 +283,14 @@ public final class ArticleViewActivity extends BaseDictionaryActivity {
                 goBack();   
                 break;
             case KeyEvent.KEYCODE_VOLUME_UP:
-                zoomIn();
+                if (!articleView.pageUp(false)) {
+                    goBack();
+                }
                 break;
             case KeyEvent.KEYCODE_VOLUME_DOWN:
-                zoomOut();
+                if (!articleView.pageDown(false)) {
+                    nextArticle();
+                };
                 break;
             default:
                 return super.onKeyDown(keyCode, event);
@@ -294,6 +298,20 @@ public final class ArticleViewActivity extends BaseDictionaryActivity {
         return true;
     }
     
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+    	//eat key ups corresponding to key downs so that volume keys don't beep
+    switch (keyCode) {
+        case KeyEvent.KEYCODE_BACK:
+        case KeyEvent.KEYCODE_VOLUME_UP:
+        case KeyEvent.KEYCODE_VOLUME_DOWN:
+            break;
+        default:
+            return super.onKeyDown(keyCode, event);
+        }
+        return true;
+    }
+        
     private boolean zoomIn() {        
         boolean zoomed = articleView.zoomIn();
         float scale = articleView.getScale();

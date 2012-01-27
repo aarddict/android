b7352762aaa273e30874b30e1da519f73e46aca3
Jeff Doozan <jeff@doozan.com>
2012-01-27 18:03:42 -0500

    [Fix] Issue #37, close keyboard when clicking done

--- src/aarddict/android/LookupActivity.java
+++ src/aarddict/android/LookupActivity.java
@@ -40,10 +40,12 @@ import android.text.TextWatcher;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
@@ -392,7 +394,18 @@ public class LookupActivity extends BaseDictionaryActivity {
             }
         };
         editText.addTextChangedListener(textWatcher);
-                
+        
+        editText.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+            	 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+            		 InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            		 inputManager.hideSoftInputFromWindow(editText.getApplicationWindowToken(), 0);
+            		 return true;
+            	 }
+            	 return false;
+            }
+        });        
+
         editText.setInputType(InputType.TYPE_CLASS_TEXT);
                 
         Button btnClear = (Button)findViewById(R.id.clearButton);
